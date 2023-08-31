7c4e118c0bdb6abef6a9b029b34770bab32f454b
ZhouWei1229 <124109194+ZhouWei1229@users.noreply.github.com>
2023-08-31 15:08:25 +0800

    deploy.sh

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/vite-vue/'
+      : '/'
+}
 export default defineConfig({
   plugins: [vue()],
 })
