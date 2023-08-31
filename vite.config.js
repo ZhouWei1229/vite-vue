7ad2e112f357e1d600f250a13ba356401c15e440
ZhouWei1229 <124109194+ZhouWei1229@users.noreply.github.com>
2023-08-31 15:21:46 +0800

    no message

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/vite-vue/'
-      : '/'
-}
 export default defineConfig({
   plugins: [vue()],
 })
